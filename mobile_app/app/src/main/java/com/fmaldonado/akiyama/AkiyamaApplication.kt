c958b8537bff35ee1430c8dbc462576431993efb
Imaguy0 <99568458+Imaguy0@users.noreply.github.com>
2023-04-26 22:00:43 -0400

    Delete mobile_app directory

--- mobile_app/app/src/main/java/com/fmaldonado/akiyama/AkiyamaApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.fmaldonado.akiyama
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class AkiyamaApplication : Application() {
-
-}
--- mobile_app/app/src/main/java/com/fmaldonado/akiyama/data/network/LatestVersionDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.fmaldonado.akiyama.data.network
-
-import com.fmaldonado.akiyama.data.models.version.GithubVersion
-import retrofit2.http.GET
-
-interface LatestVersionDataSource {
-    @GET("latest")
-    suspend fun getLatestRelease(): GithubVersion
-}
